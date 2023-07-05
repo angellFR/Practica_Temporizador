e7d98486f5ff08c9adf72b3a6f1388f005c084ec
angellFR <lafr1514@gmail.com>
2023-07-05 16:01:38 -0600

    Initial Commit

---
+++ Practica_TEMPORIZADOR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practica_TEMPORIZADOR
+//
+//  Created by Administrator on 05/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
